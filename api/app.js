715d2b963d3d906ef5ed58b5b36eb746e2b337ed
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2023-07-04 20:14:38 -0300

    formated code

--- api/app.js
+++ api/app.js
@@ -1,25 +1,16 @@
-const {
-
-    PORT
-} = require("./config");
+const { PORT } = require("./config");
 
 const express = require("express");
 
 const router = require("./router");
 
-
-
 const app = express();
 
+app.use(express.static("public"));
+app.use(express.json());
 
-app.use(express.static("public"))
-app.use(express.json())
+app.use(router);
 
-app.use(router)
-
-
-app.listen(PORT,()=>{
-    console.log(`Listening at ${PORT}\n URL: http://localhost:${PORT}`)
-})
-
-
+app.listen(PORT, () => {
+	console.log(`Listening at ${PORT}\n URL: http://localhost:${PORT}`);
+});
--- api/config.js
+++ api/config.js
@@ -1,188 +1,186 @@
-
-const ABI =[
+const ABI = [
 	{
-		"inputs": [
+		inputs: [
 			{
-				"internalType": "string",
-				"name": "imageUrl",
-				"type": "string"
+				internalType: "string",
+				name: "imageUrl",
+				type: "string",
 			},
 			{
-				"internalType": "string",
-				"name": "description",
-				"type": "string"
-			}
+				internalType: "string",
+				name: "description",
+				type: "string",
+			},
 		],
-		"name": "createPhoto",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
+		name: "createPhoto",
+		outputs: [],
+		stateMutability: "nonpayable",
+		type: "function",
 	},
 	{
-		"inputs": [
+		inputs: [
 			{
-				"internalType": "uint256",
-				"name": "id",
-				"type": "uint256"
-			}
+				internalType: "uint256",
+				name: "id",
+				type: "uint256",
+			},
 		],
-		"name": "deletePhoto",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
+		name: "deletePhoto",
+		outputs: [],
+		stateMutability: "nonpayable",
+		type: "function",
 	},
 	{
-		"inputs": [],
-		"stateMutability": "nonpayable",
-		"type": "constructor"
+		inputs: [],
+		stateMutability: "nonpayable",
+		type: "constructor",
 	},
 	{
-		"inputs": [
+		inputs: [
 			{
-				"internalType": "uint256",
-				"name": "id",
-				"type": "uint256"
+				internalType: "uint256",
+				name: "id",
+				type: "uint256",
 			},
 			{
-				"internalType": "string",
-				"name": "imageUrl",
-				"type": "string"
+				internalType: "string",
+				name: "imageUrl",
+				type: "string",
 			},
 			{
-				"internalType": "string",
-				"name": "description",
-				"type": "string"
-			}
+				internalType: "string",
+				name: "description",
+				type: "string",
+			},
 		],
-		"name": "updatePhoto",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
+		name: "updatePhoto",
+		outputs: [],
+		stateMutability: "nonpayable",
+		type: "function",
 	},
 	{
-		"inputs": [],
-		"name": "getAllPhotos",
-		"outputs": [
+		inputs: [],
+		name: "getAllPhotos",
+		outputs: [
 			{
-				"components": [
+				components: [
 					{
-						"internalType": "string",
-						"name": "imageUrl",
-						"type": "string"
+						internalType: "string",
+						name: "imageUrl",
+						type: "string",
 					},
 					{
-						"internalType": "string",
-						"name": "description",
-						"type": "string"
+						internalType: "string",
+						name: "description",
+						type: "string",
 					},
 					{
-						"internalType": "uint256",
-						"name": "id",
-						"type": "uint256"
+						internalType: "uint256",
+						name: "id",
+						type: "uint256",
 					},
 					{
-						"internalType": "uint256",
-						"name": "timestamp",
-						"type": "uint256"
-					}
+						internalType: "uint256",
+						name: "timestamp",
+						type: "uint256",
+					},
 				],
-				"internalType": "struct Crud.Photo[]",
-				"name": "",
-				"type": "tuple[]"
-			}
+				internalType: "struct Crud.Photo[]",
+				name: "",
+				type: "tuple[]",
+			},
 		],
-		"stateMutability": "view",
-		"type": "function"
+		stateMutability: "view",
+		type: "function",
 	},
 	{
-		"inputs": [],
-		"name": "idCounter",
-		"outputs": [
+		inputs: [],
+		name: "idCounter",
+		outputs: [
 			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
+				internalType: "uint256",
+				name: "",
+				type: "uint256",
+			},
 		],
-		"stateMutability": "view",
-		"type": "function"
+		stateMutability: "view",
+		type: "function",
 	},
 	{
-		"inputs": [
+		inputs: [
 			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
+				internalType: "uint256",
+				name: "",
+				type: "uint256",
+			},
 		],
-		"name": "idToPhoto",
-		"outputs": [
+		name: "idToPhoto",
+		outputs: [
 			{
-				"internalType": "string",
-				"name": "imageUrl",
-				"type": "string"
+				internalType: "string",
+				name: "imageUrl",
+				type: "string",
 			},
 			{
-				"internalType": "string",
-				"name": "description",
-				"type": "string"
+				internalType: "string",
+				name: "description",
+				type: "string",
 			},
 			{
-				"internalType": "uint256",
-				"name": "id",
-				"type": "uint256"
+				internalType: "uint256",
+				name: "id",
+				type: "uint256",
 			},
 			{
-				"internalType": "uint256",
-				"name": "timestamp",
-				"type": "uint256"
-			}
+				internalType: "uint256",
+				name: "timestamp",
+				type: "uint256",
+			},
 		],
-		"stateMutability": "view",
-		"type": "function"
+		stateMutability: "view",
+		type: "function",
 	},
 	{
-		"inputs": [],
-		"name": "owner",
-		"outputs": [
+		inputs: [],
+		name: "owner",
+		outputs: [
 			{
-				"internalType": "address",
-				"name": "",
-				"type": "address"
-			}
+				internalType: "address",
+				name: "",
+				type: "address",
+			},
 		],
-		"stateMutability": "view",
-		"type": "function"
+		stateMutability: "view",
+		type: "function",
 	},
 	{
-		"inputs": [
+		inputs: [
 			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
+				internalType: "uint256",
+				name: "",
+				type: "uint256",
+			},
 		],
-		"name": "photoExists",
-		"outputs": [
+		name: "photoExists",
+		outputs: [
 			{
-				"internalType": "bool",
-				"name": "",
-				"type": "bool"
-			}
+				internalType: "bool",
+				name: "",
+				type: "bool",
+			},
 		],
-		"stateMutability": "view",
-		"type": "function"
-	}
-]
+		stateMutability: "view",
+		type: "function",
+	},
+];
 
+const ADDRESS = "0xa1AE06e9D769fb768778FF04E541396f0Ae09370";
 
-const ADDRESS = "0xa1AE06e9D769fb768778FF04E541396f0Ae09370"
-
-require("dotenv").config()
+require("dotenv").config();
 module.exports = {
-    ABI,
-    ADDRESS,
-    FANTOM_TESTNET_RPC_URL: process.env.FANTOM_TESTNET_RPC_URL,
-    PRIVATE_KEY: process.env.PRIVATE_KEY, // use a test account. for preventing permanent fund loss
-    PORT: process.env.PORT || 3000
-}
+	ABI,
+	ADDRESS,
+	FANTOM_TESTNET_RPC_URL: process.env.FANTOM_TESTNET_RPC_URL,
+	PRIVATE_KEY: process.env.PRIVATE_KEY, // use a test account. for preventing permanent fund loss
+	PORT: process.env.PORT || 3000,
+};
