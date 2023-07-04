8f53560553958bcce5ce7f55b4ac7922497a5b26
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2023-07-04 19:50:58 -0300

    added package and creating photo working fine

--- api/app.js
+++ api/app.js
@@ -6,14 +6,15 @@ const {
     PORT
 } = require("./config");
 const {Web3} = require("web3")
-
+const HDWalletProvider = require("@truffle/hdwallet-provider")
 const express = require("express");
+
 
 const app = express();
 
 
 app.use(express.static("public"))
-
+app.use(express.json())
 
 app.get("/photos",async(req,res)=>{
     //console.log(req)
@@ -37,7 +38,23 @@ app.get("/photos",async(req,res)=>{
     res.status(200).json(photosJson)
     
 })
+app.post("/photos",async (req,res)=>{
+    const {imageUrl,description} = req.body
+    try {
+        const web3 = new Web3(new HDWalletProvider(PRIVATE_KEY,FANTOM_TESTNET_RPC_URL))
+        const wallet = web3.eth.accounts.privateKeyToAccount("0x"+PRIVATE_KEY);
+        const crud = new web3.eth.Contract(ABI,ADDRESS)
+        const tx = await crud.methods.createPhoto(imageUrl,description).send({from:wallet.address})
 
+        if(Boolean(tx)){
+            res.status(201).json({message:"Posted a Photo!"})
+        }        
+    } catch (error) {
+        
+    }
+    
+
+})
 app.get("/photos/:id",async (req,res)=>{
     const {id} = req.params
     const web3 = new Web3(FANTOM_TESTNET_RPC_URL)
