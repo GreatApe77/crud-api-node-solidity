fa3a4e513640eff591979d36b235885a33f9a7bb
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2023-07-04 16:20:55 -0300

    connection to fantom testnet working fine

--- api/app.js
+++ api/app.js
@@ -5,6 +5,7 @@ const {
 	PRIVATE_KEY,
     PORT
 } = require("./config");
+const {Web3} = require("web3")
 
 const express = require("express");
 
@@ -14,6 +15,17 @@ const app = express();
 app.use(express.static("public"))
 
 
+app.get("/api/photos",async(req,res)=>{
+    //console.log(req)
+    const web3 = new Web3(FANTOM_TESTNET_RPC_URL)
+    const crud = new web3.eth.Contract(ABI,ADDRESS)
+    const photos = await crud.methods.getAllPhotos().call()
+    console.log(photos[0].imageUrl)
+    console.log(photos[0].description)
+
+    res.send(photos.toString())
+    
+})
 
 app.listen(PORT,()=>{
     console.log(`Listening at ${PORT}\n URL: http://localhost:${PORT}`)
