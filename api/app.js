9cb62dd0c56d160bfd3002d2b56f7c8e417ce897
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2023-07-04 18:44:20 -0300

    route to retrieve all photos working fine

--- api/app.js
+++ api/app.js
@@ -15,16 +15,35 @@ const app = express();
 app.use(express.static("public"))
 
 
-app.get("/api/photos",async(req,res)=>{
+app.get("/photos",async(req,res)=>{
     //console.log(req)
     const web3 = new Web3(FANTOM_TESTNET_RPC_URL)
     const crud = new web3.eth.Contract(ABI,ADDRESS)
     const photos = await crud.methods.getAllPhotos().call()
-    console.log(photos[0].imageUrl)
-    console.log(photos[0].description)
-
-    res.send(photos.toString())
+    const photosJson = []
+    for (let i = 0; i < photos.length; i++) {
+        
+        const photo = {
+            id:Number(photos[i].id),
+            imageUrl:photos[i].imageUrl,
+            description:photos[i].description,
+            timestamp:Number(photos[i].timestamp)
+        }
+        photosJson.push(photo)
+    }
     
+    console.log(photosJson)
+
+    res.status(200).json(photosJson)
+    
+})
+
+app.get("/photos/:id",async (req,res)=>{
+    const {id} = req.params
+    const web3 = new Web3(FANTOM_TESTNET_RPC_URL)
+    const crud = new web3.eth.Contract(ABI,ADDRESS)
+    const photo = await crud.methods.idToPhoto(Number(id))
+    res.send("Sucess")
 })
 
 app.listen(PORT,()=>{
