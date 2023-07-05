26633a2b963740d938fb2aa1b7d4dd51237bfd6b
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2023-07-05 14:11:21 -0300

    rendering images

--- public/js/script.js
+++ public/js/script.js
@@ -1,12 +1,24 @@
+function getPhotos() {
+	fetch("/photos", { method: "GET" })
+		.then((data) => data.json())
+		.then((jsonData) => {
+			console.log(jsonData);
+            const posts = []
+			for (let i = 0; i < jsonData.length; i++) {
+				const photoDiv = document.createElement("div");
+				const photoImg = document.createElement("img");
+				const photoParagraph = document.createElement("p");
+                photoImg.src = jsonData[i].imageUrl
+                photoParagraph.textContent = jsonData[i].description
+                photoDiv.className = "post"
+                photoDiv.append(photoImg,photoParagraph)
+                posts.push(photoDiv)
+			}
+            for (let i = 0; i < posts.length; i++) {
+                document.querySelector(".posts").append(posts[i])
+                
+            }
+		});
+}
 
-
-
-fetch("/photos",{method:"GET"})
-.then(data => data.json())
-.then(jsonData => console.log(jsonData))
-
-
-
-fetch("/photos/2",{method:"GET"})
-.then(data => data.json())
-.then(jsonData => console.log(new Date(jsonData.timestamp)))
+getPhotos();
