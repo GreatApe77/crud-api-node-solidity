3e49ab6b82468fe61c72e7617cb9615ba8e74e73
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2023-07-04 23:23:49 -0300

    showing only current photos

--- api/router.js
+++ api/router.js
@@ -23,7 +23,10 @@ router.get("/", async (req, res) => {
 			description: photos[i].description,
 			timestamp: Number(photos[i].timestamp),
 		};
-		photosJson.push(photo);
+		if(photo.id!==0){
+
+			photosJson.push(photo);
+		}
 	}
 
 	console.log(photosJson);
@@ -88,5 +91,24 @@ router.put("/:id", async (req, res) => {
 		res.status(500).json({ message: "Internal server error" });
 	}
 });
+router.delete("/:id",async(req,res)=>{
+	const { id } = req.params;
+	try {
+		const web3 = new Web3(
+			new HDWalletProvider(PRIVATE_KEY, FANTOM_TESTNET_RPC_URL)
+		);
+		const wallet = web3.eth.accounts.privateKeyToAccount("0x" + PRIVATE_KEY);
+		const crud = new web3.eth.Contract(ABI, ADDRESS);
+		const tx = await crud.methods
+			.deletePhoto(Number(id))
+			.send({ from: wallet.address });
 
+		if (Boolean(tx)) {
+			res.status(200).json({ message: "Deleted a Photo!" });
+		}
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ message: "Internal server error" });
+	}
+})
 module.exports = router;
--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,12 @@
-console.log("HELLO it is working!")
+
+
+
+fetch("/photos",{method:"GET"})
+.then(data => data.json())
+.then(jsonData => console.log(jsonData))
+
+
+
+fetch("/photos/2",{method:"GET"})
+.then(data => data.json())
+.then(jsonData => console.log(new Date(jsonData.timestamp)))
