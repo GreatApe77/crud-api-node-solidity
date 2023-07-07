06c61b928706ecb2ccac530b74525d8e1a591cb9
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2023-07-06 23:09:58 -0300

    minor changes to all photos page

--- public/js/script.js
+++ public/js/script.js
@@ -3,21 +3,21 @@ function getPhotos() {
 		.then((data) => data.json())
 		.then((jsonData) => {
 			console.log(jsonData);
-            const posts = []
+			const posts = [];
 			for (let i = 0; i < jsonData.length; i++) {
 				const photoDiv = document.createElement("div");
 				const photoImg = document.createElement("img");
 				const photoParagraph = document.createElement("p");
-                photoImg.src = jsonData[i].imageUrl
-                photoParagraph.textContent = jsonData[i].description
-                photoDiv.className = "post"
-                photoDiv.append(photoImg,photoParagraph)
-                posts.push(photoDiv)
+				photoParagraph.className = "description";
+				photoImg.src = jsonData[i].imageUrl;
+				photoParagraph.textContent = jsonData[i].description;
+				photoDiv.className = "post";
+				photoDiv.append(photoImg, photoParagraph);
+				posts.push(photoDiv);
 			}
-            for (let i = 0; i < posts.length; i++) {
-                document.querySelector(".posts").append(posts[i])
-                
-            }
+			for (let i = 0; i < posts.length; i++) {
+				document.querySelector(".posts").append(posts[i]);
+			}
 		});
 }
 
