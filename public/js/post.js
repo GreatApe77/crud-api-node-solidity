4e2525443ad380dbfa1439a683689a57085d2775
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2023-07-10 22:47:36 -0300

    changed form functionality

--- public/js/post.js
+++ public/js/post.js
@@ -1,38 +1,52 @@
-const form = document.querySelector("#postForm")
+const submitBtn = document.querySelector("#submit-btn");
 
+async function postForm(description, imageUrl) {
+	//const descriptionInput = document.querySelector("#description-input").value;
 
-function postForm(){
-    
-    const descriptionInput = document.querySelector("#description-input").value
-    
-    const imageUrlInput = document.querySelector("#image-url-input").value
+	//const imageUrlInput = document.querySelector("#image-url-input").value;
+    if(!nomEmptyInput(imageUrl,description)) throw new Error("Input must not be empty!")
+	const data = {
+		imageUrl: imageUrl,
+		description: description,
+	};
+	try {
+		const response = await fetch("/photos", {
+			method: "POST",
+			headers: {
+				"Content-type": "application/json",
+			},
+			body: JSON.stringify(data),
+		});
+		const jsonData = await response.json();
+		return jsonData;
+	} catch (error) {
+		throw new Error(error);
+	}
+}
 
-    const data = {
-        imageUrl: imageUrlInput,
-        description: descriptionInput
+submitBtn.addEventListener("click",async   (event) => {
+	event.preventDefault();
+    submitBtn.disabled = true
+	try {
+		const descriptionInput = document.querySelector("#description-input").value;
+
+		const imageUrlInput = document.querySelector("#image-url-input").value;
+        const response = await postForm(descriptionInput,imageUrlInput)
+        alert("Posted a photo")
+        submitBtn.disabled = false
+    } catch (error) {
+        console.error(error)
+        alert("Could not post a photo")
+        submitBtn.disabled = false
     }
+	//submitBtn.disabled = true;
+	//postForm();
+});
 
-    fetch("/photos",{
-        method:"POST",
-        headers:{
-            "Content-type":"application/json"
-        },
-        body : JSON.stringify(data)
-    })
 
-    .then(res => {
-        console.log(res.status)
-        return res.json()
-    })
+function nomEmptyInput(imageUrl,description){
 
-    .then(jsonRes =>{
-
-        console.log(jsonRes)
-    })
+    return Boolean(imageUrl && description)
 
 }
 
-form.addEventListener("submit",(event)=>{
-    event.preventDefault()
-    postForm()
-})
