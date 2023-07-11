5c3c12c566633d02b4c33dc4241c1822c01b9255
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2023-07-10 23:45:29 -0300

    reversed photo order in front end

--- public/js/script-index.js
+++ public/js/script-index.js
@@ -3,14 +3,15 @@ function getPhotos() {
 		.then((data) => data.json())
 		.then((jsonData) => {
 			console.log(jsonData);
+			const reversedPhotos = jsonData.reverse()
 			const posts = [];
 			for (let i = 0; i < jsonData.length; i++) {
 				const photoDiv = document.createElement("div");
 				const photoImg = document.createElement("img");
 				const photoParagraph = document.createElement("p");
 				photoParagraph.className = "description";
-				photoImg.src = jsonData[i].imageUrl;
-				photoParagraph.textContent = jsonData[i].description;
+				photoImg.src = reversedPhotos[i].imageUrl;
+				photoParagraph.textContent = reversedPhotos[i].description;
 				photoDiv.className = "post";
 				photoDiv.append(photoImg, photoParagraph);
 				posts.push(photoDiv);
