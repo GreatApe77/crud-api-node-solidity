f97333b16e0432e451b9f621d239913fd4a78808
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2023-07-10 23:38:13 -0300

    cleared input

--- public/js/post.js
+++ public/js/post.js
@@ -4,7 +4,8 @@ async function postForm(description, imageUrl) {
 	//const descriptionInput = document.querySelector("#description-input").value;
 
 	//const imageUrlInput = document.querySelector("#image-url-input").value;
-    if(!nomEmptyInput(imageUrl,description)) throw new Error("Input must not be empty!")
+	if (!nomEmptyInput(imageUrl, description))
+		throw new Error("Input must not be empty!");
 	const data = {
 		imageUrl: imageUrl,
 		description: description,
@@ -24,29 +25,35 @@ async function postForm(description, imageUrl) {
 	}
 }
 
-submitBtn.addEventListener("click",async   (event) => {
+submitBtn.addEventListener("click", async (event) => {
 	event.preventDefault();
-    submitBtn.disabled = true
+	submitBtn.disabled = true;
+
 	try {
 		const descriptionInput = document.querySelector("#description-input").value;
 
 		const imageUrlInput = document.querySelector("#image-url-input").value;
-        const response = await postForm(descriptionInput,imageUrlInput)
-        alert("Posted a photo")
-        submitBtn.disabled = false
-    } catch (error) {
-        console.error(error)
-        alert("Could not post a photo")
-        submitBtn.disabled = false
-    }
+		const response = await postForm(descriptionInput, imageUrlInput);
+
+		submitBtn.disabled = false;
+        clearInput()
+		alert(response.message);
+	} catch (error) {
+        clearInput()
+		console.error(error);
+		alert("Could not post a photo");
+		submitBtn.disabled = false;
+	}
 	//submitBtn.disabled = true;
 	//postForm();
 });
 
-
-function nomEmptyInput(imageUrl,description){
-
-    return Boolean(imageUrl && description)
-
+function nomEmptyInput(imageUrl, description) {
+	return Boolean(imageUrl && description);
 }
 
+function clearInput() {
+	document.querySelector("#description-input").value = "";
+
+	document.querySelector("#image-url-input").value = "";
+}
