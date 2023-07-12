1983017658b31628bb757fbe776e96d3503d68fe
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2023-07-12 15:05:49 -0300

    base64 data reached server

--- api/app.js
+++ api/app.js
@@ -1,16 +1,28 @@
 const { PORT } = require("./config");
-
 const express = require("express");
-
 const router = require("./router");
-
+const multer = require("multer")
 const app = express();
+	const upload = multer()
+//app.use(express.static("public"));
+//app.use(express.json());
 
-app.use(express.static("public"));
-app.use(express.json());
 
-app.use(router);
-
+app.post("/upload",upload.single("file"),(req,res)=>{
+	
+	if(req.file){
+		
+		const buffer = req.file.buffer
+		const name = req.file.originalname
+		console.log(req.file.originalname)
+		console.log(name)
+		//console.log(buffer.toString("base64"))
+		res.send("Deu certo")
+	}else{
+		res.status(500).send("Deu errado")
+	}
+})
+//app.use(router);
 app.listen(PORT, () => {
 	console.log(`Listening at ${PORT}\nURL: http://localhost:${PORT}`);
 });
