b7cf3f3a629d9ec4b0e2e653c8b51135f96cc3a1
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2023-07-13 16:09:39 -0300

    spinner in upload

--- public/js/post.js
+++ public/js/post.js
@@ -1,86 +1,60 @@
-const submitBtn = document.querySelector("#submit-btn");
+
 const uploadForm = document.querySelector("#upload-form");
 const uploadFileInput = document.querySelector("#upload-file-input");
 const uploadDescriptionInput = document.querySelector(
 	"#upload-description-input"
 );
-async function postForm(description, imageUrl) {
-	//const descriptionInput = document.querySelector("#description-input").value;
 
-	//const imageUrlInput = document.querySelector("#image-url-input").value;
-	if (!nomEmptyInput(imageUrl, description))
-		throw new Error("Input must not be empty!");
-	const data = {
-		imageUrl: imageUrl,
-		description: description,
-	};
-	try {
-		const response = await fetch("/photos", {
-			method: "POST",
-			headers: {
-				"Content-type": "application/json",
-			},
-			body: JSON.stringify(data),
-		});
-		const jsonData = await response.json();
-		return jsonData;
-	} catch (error) {
-		throw new Error(error);
-	}
-}
 
-submitBtn.addEventListener("click", async (event) => {
-	event.preventDefault();
-	submitBtn.disabled = true;
-
-	try {
-		const descriptionInput = document.querySelector("#description-input").value;
-
-		const imageUrlInput = document.querySelector("#image-url-input").value;
-		const response = await postForm(descriptionInput, imageUrlInput);
-
-		submitBtn.disabled = false;
-		clearInput();
-		alert(response.message);
-	} catch (error) {
-		clearInput();
-		console.error(error);
-		alert("Could not post a photo");
-		submitBtn.disabled = false;
-	}
-	//submitBtn.disabled = true;
-	//postForm();
-});
-
-function nomEmptyInput(imageUrl, description) {
-	return Boolean(imageUrl && description);
-}
-
-function clearInput() {
-	document.querySelector("#description-input").value = "";
-
-	document.querySelector("#image-url-input").value = "";
-}
 
 uploadForm.addEventListener("submit", (event) => {
+
 	event.preventDefault();
+	
 	const file = uploadFileInput.files[0];
-	console.log(file);
+	
+	uploadForm.children[3].style.display = "none"
+	const spinner = createSpinner()
+	uploadForm.append(spinner)
 	const description = uploadDescriptionInput.value;
 	const formData = new FormData();
 	formData.append("description", description);
 	formData.append("file", file);
+	
+	
 	fetch("/photos", {
 		method: "POST",
 		body: formData,
 	})
 		.then((res) => {
 			if (res.status === 201) {
+				deleteSpinner(spinner)
+				uploadForm.children[3].style.display = "inline"
 				alert("Posted a photo!");
 			} else {
+				deleteSpinner(spinner)
+				uploadForm.children[3].style.display = "inline"
 				alert("Could not post a photo!");
 			}
 		})
 
 		.catch((err) => console.log(err));
 });
+
+
+function createSpinner(){
+
+
+	const mainDiv = document.createElement("div")
+	mainDiv.setAttribute("class","lds-roller")
+	for (let i = 0; i < 8; i++) {
+		mainDiv.append(document.createElement("div"))
+		
+	}
+	
+	return mainDiv
+}
+
+function deleteSpinner(spinner){
+	spinner.remove()
+}
