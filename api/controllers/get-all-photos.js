1799380d7082af7b2c368db06882ef1844a6af15
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2023-07-16 10:53:29 -0300

    removed console.log in the server and added box shadow in css

--- api/controllers/get-all-photos.js
+++ api/controllers/get-all-photos.js
@@ -1,31 +1,32 @@
 const { FANTOM_TESTNET_RPC_URL, ABI, ADDRESS } = require("../config");
-const {Web3} = require("web3");
+const { Web3 } = require("web3");
 
-
-const getAllPhotos =  async (req, res) => {
+const getAllPhotos = async (req, res) => {
 	//console.log(req)
-	const web3 = new Web3(FANTOM_TESTNET_RPC_URL);
-	const crud = new web3.eth.Contract(ABI, ADDRESS);
-	const photos = await crud.methods.getAllPhotos().call();
-	const photosJson = [];
-	for (let i = 0; i < photos.length; i++) {
-		const photo = {
-			id: Number(photos[i].id),
-			imageUrl: photos[i].imageUrl,
-			description: photos[i].description,
-			timestamp: Number(photos[i].timestamp),
-		};
-		if(photo.id!==0){
+	try {
+		const web3 = new Web3(FANTOM_TESTNET_RPC_URL);
+		const crud = new web3.eth.Contract(ABI, ADDRESS);
 
-			photosJson.push(photo);
+		const photos = await crud.methods.getAllPhotos().call();
+		const photosJson = [];
+		for (let i = 0; i < photos.length; i++) {
+			const photo = {
+				id: Number(photos[i].id),
+				imageUrl: photos[i].imageUrl,
+				description: photos[i].description,
+				timestamp: Number(photos[i].timestamp),
+			};
+			if (photo.id !== 0) {
+				photosJson.push(photo);
+			}
 		}
+		res.status(200).json(photosJson);
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ message: "Internal server error" });
 	}
-
-	console.log(photosJson);
-
-	res.status(200).json(photosJson);
 };
 
 module.exports = {
-    getAllPhotos
-}
+	getAllPhotos,
+};
--- api/controllers/get-single-photo.js
+++ api/controllers/get-single-photo.js
@@ -1,24 +1,29 @@
 const { FANTOM_TESTNET_RPC_URL, ABI, ADDRESS } = require("../config");
-const {Web3} = require("web3");
+const { Web3 } = require("web3");
 
 const getSinglePhoto = async (req, res) => {
 	const { id } = req.params;
-	const web3 = new Web3(FANTOM_TESTNET_RPC_URL);
-	const crud = new web3.eth.Contract(ABI, ADDRESS);
-	const isValidParam = await crud.methods.photoExists(Number(id)).call();
-	if (!isValidParam) {
-		return res.status(400).json({ message: "This ID Does not Exist!" });
+	try {
+		const web3 = new Web3(FANTOM_TESTNET_RPC_URL);
+		const crud = new web3.eth.Contract(ABI, ADDRESS);
+		const isValidParam = await crud.methods.photoExists(Number(id)).call();
+		if (!isValidParam) {
+			return res.status(400).json({ message: "This ID Does not Exist!" });
+		}
+		const photo = await crud.methods.idToPhoto(Number(id)).call();
+		const photoJson = {
+			id: Number(photo.id),
+			imageUrl: photo.imageUrl,
+			description: photo.description,
+			timestamp: Number(photo.timestamp),
+		};
+		res.status(200).json(photoJson);
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ message: "Internal server error" });
 	}
-	const photo = await crud.methods.idToPhoto(Number(id)).call();
-	const photoJson = {
-		id: Number(photo.id),
-		imageUrl: photo.imageUrl,
-		description: photo.description,
-		timestamp: Number(photo.timestamp),
-	};
-	res.status(200).json(photoJson);
-}
+};
 
 module.exports = {
-    getSinglePhoto
-}
+	getSinglePhoto,
+};
