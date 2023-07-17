629b21defa192494f3f8071424cf27dce5ecd343
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2023-07-17 15:10:11 -0300

    updated get single photo controller

--- api/controllers/get-single-photo.js
+++ api/controllers/get-single-photo.js
@@ -1,26 +1,26 @@
-const { FANTOM_TESTNET_RPC_URL, ABI, ADDRESS } = require("../config");
-const { Web3 } = require("web3");
+const { crudContract } = require("../config");
+
 
 const getSinglePhoto = async (req, res) => {
 	const { id } = req.params;
 	try {
-		const web3 = new Web3(FANTOM_TESTNET_RPC_URL);
-		const crud = new web3.eth.Contract(ABI, ADDRESS);
-		const isValidParam = await crud.methods.photoExists(Number(id)).call();
+		
+		
+		const isValidParam = await crudContract.methods.photoExists(Number(id)).call();
 		if (!isValidParam) {
 			return res.status(400).json({ message: "This ID Does not Exist!" });
 		}
-		const photo = await crud.methods.idToPhoto(Number(id)).call();
+		const photo = await crudContract.methods.idToPhoto(Number(id)).call();
 		const photoJson = {
 			id: Number(photo.id),
 			imageUrl: photo.imageUrl,
 			description: photo.description,
 			timestamp: Number(photo.timestamp),
 		};
-		res.status(200).json(photoJson);
+		return res.status(200).json(photoJson);
 	} catch (error) {
 		console.error(error);
-		res.status(500).json({ message: "Internal server error" });
+		return res.status(500).json({ message: "Internal server error" });
 	}
 };
 
