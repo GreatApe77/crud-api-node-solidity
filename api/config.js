c8aa0acb86c1d25800512e1131f5c9d41afb10e3
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2023-07-17 15:04:54 -0300

    update photo function new config

--- api/config.js
+++ api/config.js
@@ -1,4 +1,7 @@
 require("dotenv").config();
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const {default:Web3} = require("web3");
+
 const ABI = [
 	{
 		inputs: [
@@ -178,12 +181,34 @@ const ABI = [
 const ADDRESS = "0xa1AE06e9D769fb768778FF04E541396f0Ae09370";
 
 
+const FANTOM_TESTNET_RPC_URL = process.env.FANTOM_TESTNET_RPC_URL || "https://rpc.ankr.com/fantom_testnet/"
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const PORT = process.env.PORT ||8080
+const PASSWORD = process.env.PASSWORD
+const MORALIS_API_KEY = process.env.MORALIS_API_KEY
+
+const web3 = new Web3(new HDWalletProvider(PRIVATE_KEY,FANTOM_TESTNET_RPC_URL))
+const account = web3.eth.accounts.privateKeyToAccount("0x"+PRIVATE_KEY)
+const crudContract = new web3.eth.Contract(ABI,ADDRESS)
+
+
+module.exports = {
+	crudContract,
+	account,
+	PORT,
+	MORALIS_API_KEY,
+	PASSWORD
+}
+
+/*
 module.exports = {
 	ABI,
 	ADDRESS,
 	FANTOM_TESTNET_RPC_URL: process.env.FANTOM_TESTNET_RPC_URL,
-	PRIVATE_KEY: process.env.PRIVATE_KEY, // use a test account. for preventing permanent fund loss
+	PRIVATE_KEY: , // use a test account. for preventing permanent fund loss
 	PORT: process.env.PORT || 8080,
 	PASSWORD: process.env.PASSWORD,
 	MORALIS_API_KEY:process.env.MORALIS_API_KEY
 };
+*/
--- api/controllers/update-photo.js
+++ api/controllers/update-photo.js
@@ -1,6 +1,6 @@
-const { Web3 } = require("web3");
-const { PRIVATE_KEY, FANTOM_TESTNET_RPC_URL, ABI, ADDRESS } = require("../config");
-const HDWalletProvider = require("@truffle/hdwallet-provider")
+
+const {crudContract } = require("../config");
+
 
 
 const updatePhoto = async (req, res) => {
@@ -8,12 +8,8 @@ const updatePhoto = async (req, res) => {
 	const { imageUrl, description } = req.body;
 	console.log(req.body)
 	try {
-		const web3 = new Web3(
-			new HDWalletProvider(PRIVATE_KEY, FANTOM_TESTNET_RPC_URL)
-		);
-		const wallet = web3.eth.accounts.privateKeyToAccount("0x" + PRIVATE_KEY);
-		const crud = new web3.eth.Contract(ABI, ADDRESS);
-		const tx = await crud.methods
+		
+		const tx = await crudContract.methods
 			.updatePhoto(id,imageUrl, description)
 			.send({ from: wallet.address });
 
