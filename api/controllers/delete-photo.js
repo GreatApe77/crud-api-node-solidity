7bdaf501a6240c97433cbeabdb5442291c201ba1
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2023-07-17 22:02:05 -0300

    delete route working fine

--- api/controllers/delete-photo.js
+++ api/controllers/delete-photo.js
@@ -1,18 +1,13 @@
-const { FANTOM_TESTNET_RPC_URL, ABI,PRIVATE_KEY, ADDRESS } = require("../config");
-const {Web3} = require("web3");
-const HDWalletProvider = require("@truffle/hdwallet-provider")
+
+const {crudContract, account} = require("../config");
 
 const deletePhoto = async(req,res)=>{
 	const { id } = req.params;
 	try {
-		const web3 = new Web3(
-			new HDWalletProvider(PRIVATE_KEY, FANTOM_TESTNET_RPC_URL)
-		);
-		const wallet = web3.eth.accounts.privateKeyToAccount("0x" + PRIVATE_KEY);
-		const crud = new web3.eth.Contract(ABI, ADDRESS);
-		const tx = await crud.methods
+
+		const tx = await crudContract.methods
 			.deletePhoto(Number(id))
-			.send({ from: wallet.address });
+			.send({ from: account.address });
 
 		if (Boolean(tx)) {
 			res.status(200).json({ message: "Deleted a Photo!" });
