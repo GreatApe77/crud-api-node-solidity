edf99f16d889a4e194ea14c1245a41ff00c71a2d
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2023-07-21 20:37:36 -0300

    first idea of load more photos button

--- public/js/script-index.js
+++ public/js/script-index.js
@@ -3,32 +3,35 @@ function getPhotos() {
 		.then((data) => data.json())
 		.then((jsonData) => {
 			const photos = jsonData.reverse();
-			for (let i = 0; i < photos.length; i++) {
-				const date = new Date(photos[i].timestamp * 1000).toString();
-				const formatedDate = date.split(" ").slice(0, 5);
-				document.querySelector(".posts").innerHTML += `
-				<div class="post">
-				<img src="${photos[i].imageUrl}" alt="Img"/>
-				<p class="description">
-				${photos[i].description}
-				<p>${
-					formatedDate[0] +
-					" " +
-					formatedDate[1] +
-					" " +
-					formatedDate[2] +
-					" " +
-					formatedDate[3] +
-					" " +
-					formatedDate[4]
-				}</p>
-				</p>
-				
-				</div>
-				`;
-			}
-
+			renderPhotos(photos,photos.length)
 		});
 }
 
 getPhotos();
+
+function renderPhotos(photos, range) {
+	for (let i = 0; i < range; i++) {
+		const date = new Date(photos[i].timestamp * 1000).toString();
+		const formatedDate = date.split(" ").slice(0, 5);
+		document.querySelector(".posts").innerHTML += `
+		<div class="post">
+		<img src="${photos[i].imageUrl}" alt="Img"/>
+		<p class="description">
+		${photos[i].description}
+		<p>${
+			formatedDate[0] +
+			" " +
+			formatedDate[1] +
+			" " +
+			formatedDate[2] +
+			" " +
+			formatedDate[3] +
+			" " +
+			formatedDate[4]
+		}</p>
+		</p>
+		
+		</div>
+		`;
+	}
+}
