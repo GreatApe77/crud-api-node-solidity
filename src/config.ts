088485864b2d2d8c4e769a197e4b58f7f4c483d1
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2023-08-05 11:00:41 -0300

    first test with new config file

--- src/config.ts
+++ src/config.ts
@@ -1,23 +1,35 @@
 import dotenv from "dotenv";
-import { Web3 } from "web3";
+import { Contract, ContractAbi, Web3 } from "web3";
+import { ABI, ADDRESS } from "./contract-data";
 
-import { Provider } from "@truffle/hdwallet-provider/dist/constructor/types";
-dotenv.config()
-const FANTOM_TESTNET_RPC_URL = process.env.FANTOM_TESTNET_RPC_URL || "https://rpc.ankr.com/fantom_testnet/"
+dotenv.config();
+const FANTOM_TESTNET_RPC_URL =
+	process.env.FANTOM_TESTNET_RPC_URL || "https://rpc.ankr.com/fantom_testnet/";
 
-const PRIVATE_KEY = process.env.PRIVATE_KEY
-const PORT = process.env.PORT ||8080
-const PASSWORD = process.env.PASSWORD
-const MORALIS_API_KEY = process.env.MORALIS_API_KEY
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const PORT = process.env.PORT || 8080;
+const PASSWORD = process.env.PASSWORD;
+const MORALIS_API_KEY = process.env.MORALIS_API_KEY;
 
-const provider:Provider = new Web3.providers.HttpProvider(FANTOM_TESTNET_RPC_URL)
-const web3 = new Web3(provider)
+const web3 = new Web3(FANTOM_TESTNET_RPC_URL);
 
+const wallet = web3.eth.accounts.wallet.add("0x" + String(PRIVATE_KEY));
+const web3Account = web3.eth.accounts.privateKeyToAccount(
+	"0x" + String(PRIVATE_KEY)
+);
+
+const crudContract: Contract<ContractAbi> = new web3.eth.Contract(ABI, ADDRESS);
+
+const config = {
+	web3,
+	wallet,
+	web3Account,
+	PORT,
+	crudContract,
+};
 
 //const account = web3.eth.accounts.privateKeyToAccount("0x"+PRIVATE_KEY)
 //const crudContract = new web3.eth.Contract(ABI,ADDRESS)
-
-
 
 /*
 module.exports = {
