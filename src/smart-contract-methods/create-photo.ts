0adaf71edfb8ba7ea482b9fc19fa21543625872e
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2023-08-18 15:12:26 -0300

    documenta o create photo

--- src/smart-contract-methods/create-photo.ts
+++ src/smart-contract-methods/create-photo.ts
@@ -2,7 +2,12 @@ import { config } from "../config";
 import { TransactionReceipt } from "web3";
 
 
-
+/**
+ * o método createPhoto dos smart contract e seus parametros
+ * @param imageUrl 
+ * @param description 
+ * @returns Um objeto do tipo Transactionreceipt contendo os hashs e outras informacoes
+ */
 export async function createPhoto(imageUrl:string,description:string):Promise<TransactionReceipt>{
 
     const txReceipt:TransactionReceipt = await (config.crudContract.methods.createPhoto as any)(imageUrl,description).send({from:config.web3Account.address})
