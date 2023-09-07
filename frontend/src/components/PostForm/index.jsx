d3b7747264006f3128a42f4e12dc0a5871b659ef
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2023-09-07 10:10:38 -0300

    simulation of a form

--- frontend/src/components/PostForm/index.jsx
+++ frontend/src/components/PostForm/index.jsx
@@ -1,15 +1,38 @@
+import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Spinner from "react-bootstrap/Spinner";
+	 
+export default function PostForm({ loading,postPhoto }) {
+	
 
-export default function PostForm({ loading }) {
-    
-    
+	const [description, setDescription] = useState("")
+	const [file,setFile] = useState([])
+	function handleChange(e){
+		setDescription(e.target.value)
+	}
+	function handleFileInput(e){
+		setFile(e.target.files[0])
+	}
+    function handleSubmit(e){
+		e.preventDefault()
+		postPhoto(description,file)
+		.then((res)=>{
+			
+			console.log(res)
+			console.log(file)
+			
+			
+		})
+		.catch((err)=>{
+			console.log(err)
+		})
+	}
 	return (
-		<>
+		<div>
 			<h1 className="text-white mb-5 mt-5">Post a Photo!</h1>
 			<div>
-				<Form>
+				<Form onSubmit={handleSubmit}>
 					<fieldset disabled={loading}>
 						<Form.Group className="mb-3" controlId="formBasicEmail">
 							<Form.Label className="text-white">Description:</Form.Label>
@@ -17,12 +40,13 @@ export default function PostForm({ loading }) {
 								type="text"
 								placeholder="Type The Description"
 								required
+								onChange={handleChange}
 							/>
 						</Form.Group>
 
 						<Form.Group controlId="formFile" className="mb-3">
 							<Form.Label className="text-white">Photo:</Form.Label>
-							<Form.Control type="file" required />
+							<Form.Control type="file" required  onChange={handleFileInput}/>
 						</Form.Group>
 
 						<Button variant="success" className="mx-auto" type="submit">
@@ -38,6 +62,6 @@ export default function PostForm({ loading }) {
 					</fieldset>
 				</Form>
 			</div>
-		</>
+		</div>
 	);
 }
--- frontend/src/routes/Post.jsx
+++ frontend/src/routes/Post.jsx
@@ -1,13 +1,29 @@
+import { useState } from "react"
 import PostForm from "../components/PostForm"
 
 
 function Post(){
-   
+    const [loading, setLoading] = useState(false)
+    function postPhoto(description,file){
+        return new Promise((resolve,reject)=>{
+            setLoading(true)
+            if(description&&file){
+                setTimeout(()=>{
+                    setLoading(false)
+                    resolve(`Description: ${description} and file is here too`)
+                },3000)
+            }else{
+                setLoading(false)
+                reject("No Description !")
+            }
+            
+        })
+    }
     return(
         <div className="d-flex flex-column justify-content-center ">
             <div className="mx-auto">
 
-            <PostForm/>
+            <PostForm loading={loading} postPhoto={postPhoto} />
             </div>
         
         </div>
